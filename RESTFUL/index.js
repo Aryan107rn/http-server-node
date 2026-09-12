1606c614108d5d4557278cda62cd8f1e12027f2b
Aryan107rn <aryannakade123@gmail.com>
2026-09-13 00:27:52 +0530

    /user ->json data on web and /api/user gives unordered list data on web

--- RESTFUL/index.js
+++ RESTFUL/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 const app=express();
 const PORT=8000;
 
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+});
 
+app.get('/users',(req,res)=>{
+    const html='
+    ,<ul>
+    ${users.map((user)=>'<li>${user.first_name}</li>')}
+    </ul>'
+});
 app.listen(PORT,()=>console.log('server started and port '+$`PORT`));
