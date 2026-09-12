6923d18fd14224e0e3e76f0aa762b535662af165
Aryan107rn <aryannakade123@gmail.com>
2026-09-13 00:53:33 +0530

    :id dynamically added user

--- RESTFUL/index.js
+++ RESTFUL/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
-const app=express();
-const PORT=8000;
 
-app.get('/api/users',(req,res)=>{
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+
+const PORT = 8000;
+
+app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app.get('/users',(req,res)=>{
-    const html='
-    ,<ul>
-    ${users.map((user)=>'<li>${user.first_name}</li>')}
-    </ul>'
+app.get("/users", (req, res) => {
+
+    const html = `
+        <ul>
+            ${users
+                .map((user) => `<li>${user.first_name}</li>`)
+                .join("")}
+        </ul>
+    `;
+
+    return res.send(html);
 });
-app.listen(PORT,()=>console.log('server started and port '+$`PORT`));
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = req.params.id;
+    const user = user.find((user)=>user.id===id)
+});
+// :id is dynamic means 1,2,3,4,5,......
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
