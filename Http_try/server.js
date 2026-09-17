9aad12a60cb5761bc9b45dae0671b4507cfd7481
Aryan107rn <aryannakade123@gmail.com>
2026-09-17 15:01:20 +0530

    completed

--- Http_try/server.js
+++ Http_try/server.js
@@ -7,4 +7,15 @@ const server =http.createServer((req,res)=>{
     else if(req.url==="/users" && req.method==="GET"){
         res.send("User page");
     }
+    else if(req.url==="/about" && req.method==="GET"){
+        res.send("About Page");
+    }
+    else{
+        res.statusCode=404;
+        res.end("Not found");
+    }
+});
+
+server.listen(3000,()=>{
+    console.log("Server running on port 3000");
 })
